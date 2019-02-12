52185c77b177a9180a01126f47aca241fa212416
Deie <chantalsauvann@gmail.com>
2019-02-12 13:02:13 +0100

    add nav links & categories hbs files

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const Category = require('../models/category-model.js')
 
 // HOMEPAGE ROUTER --------------------------------------------------------------
 //####################################################################
-/* GET home page */
+  //GET TO HOMEPAGE ****************************************
 router.get("/", (req, res, next) => {
   res.render("index");
 });
@@ -17,6 +17,19 @@ router.get("/incivility-category", (req, res, next) => {
     .then(categoryResults => {
       res.locals.categoryArray = categoryResults;
       res.render("incivilities-views/inc-categories.hbs");
+    })
+
+    // next(err) skips to the error handler in "bin/wwww" (error.hbs)
+    .catch(err => next(err));
+});
+
+//HOMEPAGE TO INCIVILITIES CATEGORY ****************************************
+router.get("/request-category", (req, res, next) => {
+  Category.find()
+
+    .then(categoryResults => {
+      res.locals.categoryArray = categoryResults;
+      res.render("requests-views/req-categories.hbs");
     })
 
     // next(err) skips to the error handler in "bin/wwww" (error.hbs)

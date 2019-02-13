1c3bb875f86f91ce9afbfe1b6446a48774ffbbdc
Deie <chantalsauvann@gmail.com>
2019-02-13 21:51:21 +0100

    user account routes

--- routes/user-account-router.js
+++ routes/user-account-router.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const User = require("../models/user-model");
 const router = express.Router();
+const Request = require("../models/request-model");
 
 
 // USER ACCOUNT ROUTER --------------------------------------------------------------
 //####################################################################
 
 // ACCESS TO USER ACCOUNT PAGE --------------------------------------------------------------
-router.get("/mon-compte", (req, res, next) => {
+router.get("/mon-compte/:id", (req, res, next) => {
   // enforce login rules to access create a room ****************************************************
   if (!req.user) {
     req.flash("logError", `You have to be logged in ! 🥣`);
-    //redirect to login page if userResult is NULL (no account with the email)
     res.redirect("/identification");
+  } else {   
+    const userRequest = req.user._id;  
+      // GET USER'S INFO **************************************************
+      User.findById(req.params.id)
+      .then(userDoc => {
+        res.locals.userDoc = userDoc;
+        res.redirect("user-account-views/user-account" + userDoc._id);
+      })
+      .then(
+        // GET USER'S REQUEST **************************************************
+        Request.findById(userRequest)
+        .sort({ createdAt: -1 })
+        .then(reqUser => {
+          res.locals.reqUser = reqUser;
+          res.render("user-account-views/user-account");    
+        })
+      )
+      .catch(err => next(err));
+
+    
   }
   //*********************************************************************
 
-  // find user in databse using the ID from the url **************************************************
 
-  User.findById(req.user._id)
-    .then(userDoc => {
-      res.locals.userDoc = userDoc;
-      res.render("user-account-views/user-account");
-    })
-    .catch(err => next(err));
+  //GET TO USER'S REQUEST LISTING ****************************************
+ 
+
   //*********************************************************************
   // END SHOW USER PROFIL --------------------------------------------------------------
 });
